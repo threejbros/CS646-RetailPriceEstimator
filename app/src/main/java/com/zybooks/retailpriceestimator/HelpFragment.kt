014c5900636bee1743000bc51a7ddcf68586e5c8
Jonathan Lee <threejbros@gmail.com>
2023-05-08 21:35:18 -0700

    2.4 cleaned up bounce animation and helpFragment.xml

--- app/src/main/java/com/zybooks/retailpriceestimator/HelpFragment.kt
+++ app/src/main/java/com/zybooks/retailpriceestimator/HelpFragment.kt
@@ -16,34 +16,11 @@ class HelpFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val parentView = inflater.inflate(R.layout.fragment_help, container, false)
 
-//        val promoImage = parentView.findViewById<ImageView>(R.id.promoImage)
-//        val animator = ValueAnimator.ofFloat(-100f, 200f)
-//        animator.duration = 1000
-//        animator.interpolator = BounceInterpolator()
-////        animator.addUpdateListener { animation: ValueAnimator ->
-////            promoImage.baseline = animation.animatedValue as Int
-////        }
-//
-//        animator.start()
-
-        //Rotation for promoImage
-//        val animator = ObjectAnimator.ofFloat(promoImage, "rotation", 0f, 360f)
-//        animator.duration = 500
-//
-//        animator.start()
-
         val bounceAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.bounce_animation)
         val promoImage = parentView.findViewById<ImageView>(R.id.promoImage)
 
         promoImage.startAnimation(bounceAnimation)
 
         return parentView
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
     }
 }
