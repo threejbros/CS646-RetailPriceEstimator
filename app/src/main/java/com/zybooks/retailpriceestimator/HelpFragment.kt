eeb4c5fce00fcab91ecfa8c2474522b8ca79c282
Jonathan Lee <threejbros@gmail.com>
2023-05-08 21:23:43 -0700

    2.3 added animation to promo. and there is some extra stuff in HelpFragment that will be removed in later revisions

--- app/src/main/java/com/zybooks/retailpriceestimator/HelpFragment.kt
+++ app/src/main/java/com/zybooks/retailpriceestimator/HelpFragment.kt
@@ -1,14 +1,49 @@
 package com.zybooks.retailpriceestimator
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.view.animation.BounceInterpolator
+import android.widget.ImageView
 
 class HelpFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_help, container, false)
+        val parentView = inflater.inflate(R.layout.fragment_help, container, false)
+
+//        val promoImage = parentView.findViewById<ImageView>(R.id.promoImage)
+//        val animator = ValueAnimator.ofFloat(-100f, 200f)
+//        animator.duration = 1000
+//        animator.interpolator = BounceInterpolator()
+////        animator.addUpdateListener { animation: ValueAnimator ->
+////            promoImage.baseline = animation.animatedValue as Int
+////        }
+//
+//        animator.start()
+
+        //Rotation for promoImage
+//        val animator = ObjectAnimator.ofFloat(promoImage, "rotation", 0f, 360f)
+//        animator.duration = 500
+//
+//        animator.start()
+
+        val bounceAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.bounce_animation)
+        val promoImage = parentView.findViewById<ImageView>(R.id.promoImage)
+
+        promoImage.startAnimation(bounceAnimation)
+
+        return parentView
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
     }
 }
